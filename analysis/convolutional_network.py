b0c5b170a1de8ef05d9564d7a2741285f7b32025
zachweed <1937294+zachweed@users.noreply.github.com>
2025-09-29 11:02:55 -0400

    Update convolutional_network.py

--- analysis/convolutional_network.py
+++ analysis/convolutional_network.py
@@ -4,5 +4,5 @@ class ConvolutionalNetwork:
   def laplacian_kernel(self, neighbors):
     if neighbors == 8:
       return np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]], dtype=np.float32)
-    else
+    else:
       return np.array([[0, 1, 0], [1, -4, 1], [0, 1, 0]], dtype=np.float32)
