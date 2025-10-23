8464bf7be500ea7c4a215163f717950d9582fb6d
Zachary Weed <zweed@Zacharys-MBP-2.lan>
2025-10-23 09:20:38 -0400

    handling input builder

--- analysis/input_builder.py
+++ analysis/input_builder.py
@@ -1,1 +1,13 @@
-input_builder.py
+class InputBuilder:
+  def __init__(self, inputs, layers):
+    self.inputs = inputs
+    self.layers = layers
+
+  def build(self, training = None):
+    x = self.inputs
+    for layer in self.layers:
+      try:
+        x = layer(x, training = training)
+      except TypeError:
+        x = layer(x)
+    return x
